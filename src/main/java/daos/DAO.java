c0e2128df2160e280f7acfe323208e121aa9adac
julidann <169107108+julidann@users.noreply.github.com>
2026-08-31 16:21:21 -0300

    Creo interface DAO

--- src/main/java/daos/DAO.java
+++ src/main/java/daos/DAO.java
@@ -1,4 +1,20 @@
 package daos;
 
-public class DAO {
+package daos;
+
+import java.util.List;
+
+public interface DAO<T> {
+
+    T findById(int id);
+
+    List<T> findAll();
+
+    void create(T entity);
+
+    void update(T entity);
+
+    void delete(int id);
+
+    void deleteAll();
 }
